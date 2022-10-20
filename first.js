d04f4cc910b141dc58f10288c5b16ca3dc4dc00c
Adeshvir Dhanoa <ad13dhanoa@gmail.com>
2022-10-19 21:13:07 -0400

    added js

---
+++ first.js
@@ -0,0 +1,44 @@
+// let num1 = 6;
+// let num2 = 4;
+
+// let result = num1 > num2;
+
+// if(result)
+// console.log("num1 is greater"); 
+
+
+// console.log('Byee');
+
+// let num1= 3;
+// let num2 = 4;
+
+// if(num1 > num2) {
+// console.log("num1 is greater");
+// }
+
+// else
+//{
+// console.log("num2 is greater");
+//console.log("yee");
+//}
+// console.log('Byee');
+// 
+
+let num1= 6;
+let num2 = 4;
+let num3 = 7;
+
+
+if(num1 > num2 && num1 >num3) {
+console.log("num1 is greatest");
+}
+
+else if (num2 > num3) {
+console.log("num2 is greatest");
+}
+
+else {
+    console.log("num3 is greatest");
+}
+
+console.log('Byee');
